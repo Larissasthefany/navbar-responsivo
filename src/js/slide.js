fd702baad84a69ca2d5ce67fe6d91fe83bc8216c
Larissa Sthefany <110210572+Larissasthefany@users.noreply.github.com>
2023-03-08 23:24:47 -0300

    Carrossel

--- src/js/slide.js
+++ src/js/slide.js
@@ -1,60 +1,41 @@
-const imagens = document.querySelectorAll(".slide")
-const setaVoltar = document.getElementById("seta-voltar")
-const setaAvancar = document.getElementById("seta-avancar")
+const slider = document.querySelectorAll(".slider")
+const btnPrev = document.getElementById("prev-button")
+const btnNext = document.getElementById("next-button")
 
-let imagemAtual = 0;
+let currentSlide = 0;
 
-setaAvancar.addEventListener("click", function (){
-
-    if(imagemAtual === imagens.length - 1) {
-        return;
-    }
-
-    imagemAtual++;
-
-    esconderImagemAberta();
-    // imagens[imagemAtual].classList.add("mostrar")
-    mostrarImagem();
-    mostrarOuEsconderSetas();
-})
-
-setaVoltar.addEventListener("click", function (){
-    if(imagemAtual === 0){
-        return;
-    }
-
-    imagemAtual--;
-
-    esconderImagemAberta();
-    mostrarImagem();
-    mostrarOuEsconderSetas();
-})
-
-function mostrarImagem() {
-    imagens[imagemAtual].classList.add("mostrar");
+function hideSlider(){
+    slider.forEach(item => item.classList.remove('on'))
 }
 
-function mostrarImagem() {
-    imagens[imagemAtual].classList.add("mostrar");
+function showSlider(){
+    slider[currentSlide].classList.add('on')
 }
 
-function esconderImagemAberta(){
-    const imagemAberta = document.querySelector(".mostrar");
-    imagemAberta.classList.remove("mostrar");
-}
+function nextSlider(){
+    hideSlider()
 
-function mostrarOuEsconderSetas() {
-    const naoEAprimeiraImagem = imagemAtual !== 0;
-    if(naoEAprimeiraImagem) {
-        setaVoltar.classList.remove("opacidade");
-    }else {
-        setaAvancar.classList.add("opacidade");
+    if(currentSlide === slider.length -1){
+        currentSlide = 0
+    } else {
+        currentSlide++
     }
 
-    const chegouNaUltimaImagem = imagemAtual !== 0 && imagemAtual === imagens.length - 1;
-    if(chegouNaUltimaImagem) {
-        setaAvancar.classList.add("opacidade");
-    }else {
-        setaVoltar.classList.remove("opacidade");
-    }
+    showSlider()
 }
+
+function prevtSlider(){
+    hideSlider()
+
+    if(currentSlide === 0){
+        currentSlide = slider.length - 1
+    } else {
+        currentSlide--
+    }
+
+    showSlider()
+}
+
+btnNext.addEventListener("click", nextSlider)
+
+btnPrev.addEventListener("click", prevtSlider)
